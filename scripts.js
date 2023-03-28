c87efb5377d472a37bf80e318075b1fbb0aadf31
Chris Keilberg <83353741+ckeilberg57@users.noreply.github.com>
2023-03-28 10:24:59 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,92 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const scheduleBtn = document.getElementById('scheduleBtn');
+    const appointmentForm = document.getElementById('appointmentForm');
+    const alias = document.getElementById('alias');
+    const firstName = document.getElementById('firstName');
+    const lastName = document.getElementById('lastName');
+    const datePicker = document.getElementById('datePicker');
+    const email = document.getElementById('email');
+    const phone = document.getElementById('phone');
+    const submitBtn = document.getElementById('submitBtn');
+    const calendar = document.getElementById('calendar');
+    const appointmentModal = document.getElementById('appointmentModal');
+    const appointmentSummary = document.getElementById('appointmentSummary');
+    const smsReminderBtn = document.getElementById('smsReminderBtn');
+
+    function generateRandomAlias() {
+        let result = '';
+        const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+        const charactersLength = characters.length;
+        for (let i = 0; i < 12; i++) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+    }
+
+    function getRandomColor() {
+        return '#' + Math.floor(Math.random() * 16777215).toString(16);
+    }
+
+    function createAppointmentDiv(date, time) {
+        const div = document.createElement('div');
+        div.style.backgroundColor = getRandomColor();
+        div.style.padding = '10px';
+        div.style.margin = '10px';
+        div.style.cursor = 'pointer';
+        div.innerHTML = `Hello, for St. Jude's hospital. ${firstName.value} has a consultation at ${date} and ${time}. Please call XXXXXXXX if cancelation or reschedule is needed.`;
+        div.addEventListener('dblclick', () => {
+            appointmentSummary.textContent = `Appointment Summary:\nPatient: ${firstName.value} ${lastName.value}\nDate: ${date}\nTime: ${time}\nEmail: ${email.value}\nPhone: ${phone.value}`;
+            appointmentModal.classList.remove('hidden');
+        });
+        return div;
+    }
+
+    function closeModal() {
+        appointmentModal.classList.add('hidden');
+    }
+
+    scheduleBtn.addEventListener('click', () => {
+        appointmentForm.classList.toggle('hidden');
+        alias.textContent = generateRandomAlias();
+    });
+
+    const flatpickrInstance = flatpickr(datePicker, {
+        enableTime: true,
+        dateFormat: "Y-m-d H:i",
+        time_24hr: true
+    });
+
+    submitBtn.addEventListener('click', () => {
+        const selectedDate = flatpickrInstance.selectedDates[0];
+        if (!firstName.value || !lastName.value || !selectedDate || !email.value || !phone.value) {
+            alert("Please fill in all fields and select a date and time.");
+            return;
+        }
+
+        const date = selectedDate.toLocaleDateString();
+        const time = selectedDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+        const appointmentDiv = createAppointmentDiv(date, time);
+        calendar.appendChild(appointmentDiv);
+
+        firstName.value = '';
+        lastName.value = '';
+        flatpickrInstance.clear();
+        email.value = '';
+        phone.value = '';
+        appointmentForm.classList.add('hidden');
+    });
+
+    smsReminderBtn.addEventListener('click', () => {
+        alert('SMS reminder sent to patient.');
+        closeModal();
+    });
+
+    window.addEventListener('click', (event) => {
+        if (event.target == appointmentModal) {
+            closeModal();
+        }
+    });
+});
+
+
