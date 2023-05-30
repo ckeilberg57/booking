cc08e5be9c5df9da4136d16e9a3daeed689fcfb1
Chris Keilberg <83353741+ckeilberg57@users.noreply.github.com>
2023-05-30 16:50:56 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -122,7 +122,7 @@
 		evt.preventDefault();
 
 		let joinUrl = this.modal.querySelector('a.join-link').getAttribute('href');
-		let message = `${"data-conference-name"} your appointment with Dr. Demo is getting ready to begin. Please join by clicking the link: ${joinUrl}`;
+		let message = `Your appointment with Dr. Demo is getting ready to begin. Please join by clicking the link: ${joinUrl}`;
 		this.sendSmsMessage(this.modal.getAttribute("data-participant-phone"), message);
 	}
 
